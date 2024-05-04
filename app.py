7e33783e1156854b8f2e8be422c9e1a151eb94ea
unknown <sachthemaster@gmail.com>
2024-05-04 19:51:17 +0530

    All files excep .pkl Pushed

---
+++ app.py
@@ -0,0 +1,41 @@
+
+from fastapi import FastAPI
+import uvicorn
+from transformers import pipeline
+
+import pickle
+with open('HF_summarizer.pkl', 'rb') as f:
+    HF_summarizer = pickle.load(f)
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def welcome():
+    return ({"messages" : "Hello World !!!!"})
+
+@app.post('/infer')
+def get_inference(data:str):
+
+    answer = HF_summarizer(data)
+    return answer[0]['summary_text']
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port = 8000)
+
+### Note that it took me about 3 hours to figure out that the model MUST be in 
+### pickle format if we want to write code on .py file using a summarizer model
+### pipe = pipeline(task = "summarization")
+### pipe(any_string_data) will work in .ipynb
+### But to use the model as an API, it has to be in a pickle file
+
+
+
+
+
+
+
+
+
